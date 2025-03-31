34acafdec97e7340bbe86893560c565e6af2aadb
Jonathan <jrsjackson26@gmail.com>
2025-03-31 11:13:27 -0600

    added revyl icon and HomeScreenInfo.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
+import HomeScreenInfo from '@/components/HomeScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
@@ -8,7 +8,7 @@ export default function TabOneScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <HomeScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
